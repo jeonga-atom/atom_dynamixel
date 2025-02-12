35133fbe14db77a343a98bd57fb52e43b875b759
JuneHappyE <chuasd328@gmail.com>
2025-02-12 13:55:14 +0900

    다이나믹셀 코드 1 수정
    모터 추가분 갯수 반영 및 Ackermann 각도 계산 함수 임시 추가

--- Dynamixel_control_2.py
+++ Dynamixel_control_2.py
@@ -1,7 +1,8 @@
-# 250206 - 2개는 각도(위치) 제어 / 2개 속도 제어
+# 250211 - 2개는 각도(위치) 제어 / 4개 속도 제어
 
 
 import os
+import math
 
 if os.name == 'nt':
     import msvcrt
@@ -43,23 +44,35 @@ LEN_XM430_PRESENT_POSITION      = 4
 PROTOCOL_VERSION                = 2.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
-THE_NUMBER_OF_DXL               = 4
+THE_NUMBER_OF_DXL               = 6
 DXL1_ID                         = 1                 # Dynamixel#1 ID : 1
 DXL2_ID                         = 2                 # Dynamixel#1 ID : 2
+DXL3_ID                         = 3                 # Dynamixel#1 ID : 3
+DXL4_ID                         = 4                 # Dynamixel#1 ID : 4
 DXL5_ID                         = 5                 # Dynamixel#1 ID : 5
 DXL6_ID                         = 6                 # Dynamixel#1 ID : 6
 BAUDRATE                        = 1000000           # Dynamixel default baudrate : 57600
 DEVICENAME                      = '/dev/ttyACM0'    # Check which port is being used on your controller
                                                     # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
+OP_MODE_CURRENT                 = 0
 OP_MODE_VELOCITY                = 1
 OP_MODE_POSITION                = 3
+OP_MODE_EXTENDED_POS            = 4
+OP_MODE_CUR_BASED_POS           = 5
+# OP_MODE_PWM                     = 16
 
 TORQUE_ENABLE                   = 1                 # Value for enabling the torque
 TORQUE_DISABLE                  = 0                 # Value for disabling the torque
 DXL_MINIMUM_POSITION_VALUE      = 1536
 DXL_MINIMUM_POSITION_VALUE      = 2560
 DXL_MOVING_STATUS_THRESHOLD     = 5                 # Dynamixel moving status threshold
+
+DXL_DEFAULT_GO_STRAIGHT_POS     = 2048              # 차량 직진 시 다이나믹셀 기본 위치(각도)값
+
+MOBILITY_WHEELBASE              = 200               # 휠베이스(전후간 차축 수평 거리)
+MOBILITY_TREAD                  = 150               # 차륜 거리(윤거, 양쪽 차륜 수평 거리)
+
 
 
 
@@ -82,16 +95,16 @@ groupBulkRead = GroupBulkRead(portHandler, packetHandler)
 
 
 # Initialize GroupSyncWrite instance
-groupSyncWrite_GP = GroupSyncWrite(portHandler, packetHandler, ADDR_XM430_GOAL_POSITION, LEN_XM430_GOAL_POSITION)
+groupSyncWrite_GoalP = GroupSyncWrite(portHandler, packetHandler, ADDR_XM430_GOAL_POSITION, LEN_XM430_GOAL_POSITION)
 
 # Initialize GroupSyncRead instace for Present Position
-groupSyncRead_PP = GroupSyncRead(portHandler, packetHandler, ADDR_XM430_PRESENT_POSITION, LEN_XM430_PRESENT_POSITION)
+groupSyncRead_PresP = GroupSyncRead(portHandler, packetHandler, ADDR_XM430_PRESENT_POSITION, LEN_XM430_PRESENT_POSITION)
 
 # Initialize GroupSyncWrite instance for Goal Velocity
-groupSyncWrite_GV = GroupSyncWrite(portHandler, packetHandler, ADDR_XM430_GOAL_VELOCITY, LEN_XM430_GOAL_VELOCITY)
+groupSyncWrite_GoalV = GroupSyncWrite(portHandler, packetHandler, ADDR_XM430_GOAL_VELOCITY, LEN_XM430_GOAL_VELOCITY)
 
 # Initialize GroupSyncRead instace for Present Velocity
-groupSyncRead_PV = GroupSyncRead(portHandler, packetHandler, ADDR_XM430_PRESENT_VELOCITY, LEN_XM430_PRESENT_VELOCITY)
+groupSyncRead_PresV = GroupSyncRead(portHandler, packetHandler, ADDR_XM430_PRESENT_VELOCITY, LEN_XM430_PRESENT_VELOCITY)
 
 
 
@@ -158,10 +171,13 @@ def print_OP_mode(ID_):
     elif dxl_error != 0:
         print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-    
-    dxl_present_OP_print = exchange_value_of_OP_mode(OP_mode_value= dxl_present_OP_mode)
+    if dxl_comm_result == COMM_SUCCESS:
+        dxl_present_OP_print = exchange_value_of_OP_mode(OP_mode_value= dxl_present_OP_mode)
 
-    print("Present Operating mode of Dynamixel ID %03d: %s" % (ID_, dxl_present_OP_print))
+        print("Present Operating mode of Dynamixel ID %03d: %s" % (ID_, dxl_present_OP_print))
+    else:
+        print("Present Operating mode of Dynamixel ID %03d: Failed to load" % (ID_))
+
 
 
     return dxl_present_OP_print
@@ -211,15 +227,30 @@ def change_OP_mode(ID_c, Mode_set_to = None):
 
             elif mode_key in [chr(0x42), chr(0x62)]: # B, b key
                 print("Dynamixel ID %03d will change to Current-based Position mode." % ID_c)
-                pass
+                dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID_c, ADDR_XM430_OPERATING_MODE, OP_MODE_CUR_BASED_POS)
+                if dxl_comm_result != COMM_SUCCESS:
+                    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+                elif dxl_error != 0:
+                    print("%s" % packetHandler.getRxPacketError(dxl_error))
+                break
 
             elif mode_key in [chr(0x45), chr(0x65)]: # E, e key
                 print("Dynamixel ID %03d will change to Extended Position mode." % ID_c)
-                pass
+                dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID_c, ADDR_XM430_OPERATING_MODE, OP_MODE_EXTENDED_POS)
+                if dxl_comm_result != COMM_SUCCESS:
+                    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+                elif dxl_error != 0:
+                    print("%s" % packetHandler.getRxPacketError(dxl_error))
+                break
 
             elif mode_key in [chr(0x43), chr(0x63)]: # C, c key
                 print("Dynamixel ID %03d will change to Current mode." % ID_c)
-                pass
+                dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID_c, ADDR_XM430_OPERATING_MODE, OP_MODE_CURRENT)
+                if dxl_comm_result != COMM_SUCCESS:
+                    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+                elif dxl_error != 0:
+                    print("%s" % packetHandler.getRxPacketError(dxl_error))
+                break
 
             elif mode_key == chr(0x0D): # Enter key
                 print("Dynamixel ID %03d will keep the OP mode( %s )." % (ID_c, dxl_present_OP_value))
@@ -245,6 +276,17 @@ def change_OP_mode(ID_c, Mode_set_to = None):
 
     return dxl_present_OP_value
 
+
+
+def Ackermann_steering_Radius(standard_Angle_in4096: int):
+    '''차량의 회전 중심점부터 차량 차체의 중심선까지의 거리를 계산하는 함수. standard_Angle_in4096는 전진 방향을 다이나믹셀 내장값 기준 2048로 보고, 그 값보다 크면 '''
+    if not (standard_Angle_in4096 % 1024 == 0):
+        MOBILITY_center_Radius = ((200 / math.tan((standard_Angle_in4096 * (360 / 4096)) / (180 / math.pi))) 
+                                  + (MOBILITY_TREAD / 2)) # R: 차량 중심선 기준의 회전 반경
+    
+    else: # 값이 -2048(2048), -1024, 0, 1024, 2048 일 때, if문 첫 줄 계산이 불가함. 그런 경우 본 else문 실행.
+        if standard_Angle_in4096 == 0:
+            pass
 
 
 def allocate_value_into_byte_array(Target_value):
@@ -283,7 +325,7 @@ def gBulkWrite(ID_list: list[int], ADDR_list: list[int], addr_LEN_list: list[int
 #===== ===== ===== ===== ===== 함수 끝 ===== ===== ===== ===== =====#
 
 
-list_of_DXL = [DXL1_ID, DXL2_ID, DXL5_ID, DXL6_ID]
+list_of_DXL = [DXL1_ID, DXL2_ID, DXL3_ID, DXL4_ID, DXL5_ID, DXL6_ID]
 
 # Open port
 if portHandler.openPort():
@@ -312,10 +354,12 @@ execute_control = Enable_openPort and Enable_setBaudrate
 
 if execute_control:
 
-    change_OP_mode(ID_c= DXL1_ID, Mode_set_to= "Position")
+    change_OP_mode(ID_c= DXL1_ID, Mode_set_to= "Velocity")
     change_OP_mode(ID_c= DXL2_ID, Mode_set_to= "Velocity")
+    change_OP_mode(ID_c= DXL3_ID, Mode_set_to= "Velocity")
+    change_OP_mode(ID_c= DXL4_ID, Mode_set_to= "Velocity")
     change_OP_mode(ID_c= DXL5_ID, Mode_set_to= "Position")
-    change_OP_mode(ID_c= DXL6_ID, Mode_set_to= "Velocity")
+    change_OP_mode(ID_c= DXL6_ID, Mode_set_to= "Position")
 
     for ID_index in list_of_DXL:
         set_DXL_Torque(DXL_ID= ID_index, Torque_value= TORQUE_ENABLE)
@@ -349,9 +393,12 @@ else:
     print("Dynamixel#%d has been successfully connected" % DXL1_ID)
 '''
 
-Target_Position = 2048
+Target_Position = DXL_DEFAULT_GO_STRAIGHT_POS
 Target_Velocity = 0
+Ackermann_Pos = 0
+Ackermann_Vel = 0
 
+print("Press any key to continue! (or press Backspace to quit!)")
 
 while execute_control:
 
@@ -360,34 +407,33 @@ while execute_control:
     # Clear bulkwrite parameter storage
     groupBulkWrite.clearParam()
 
-    print("Press any key to continue! (or press Backspace to quit!)")
     controlling_key = getch()
 
     if controlling_key == chr(0x7F): # Backspace key
-        print("Quit!\n")
+        print("\nQuit!\n")
         break
 
 
     if controlling_key == chr(0x77): # w key(+Velocity↑)
         Target_Velocity += 4
-        if Target_Position < 2048:
-            Target_Position += 4
-        elif Target_Position > 2048:
+        if Target_Position > DXL_DEFAULT_GO_STRAIGHT_POS:
             Target_Position -= 4
+        elif Target_Position < DXL_DEFAULT_GO_STRAIGHT_POS:
+            Target_Position += 4
         else:
             pass
 
     elif controlling_key == chr(0x57): # W key(Steer wheels to go straight)
-        Target_Position = 2048
+        Target_Position = DXL_DEFAULT_GO_STRAIGHT_POS
 
     elif controlling_key == chr(0x73): # s key(-Velocity↓)
         Target_Velocity -= 4
 
     elif controlling_key == chr(0x61): # a key(Turn Left)
-        Target_Position += 16
+        Target_Position -= 16
 
     elif controlling_key == chr(0x64): # d key(Turn Right)
-        Target_Position -= 16
+        Target_Position += 16
 
     elif controlling_key == chr(0x71): # q key(Turn Left & +Velocity)
         Target_Position -= 8
@@ -417,24 +463,24 @@ while execute_control:
         Target_Velocity = 0
 
     else:
-        if Target_Position < 2048:
-            Target_Position += 2
-        elif Target_Position > 2048:
+        if Target_Position > DXL_DEFAULT_GO_STRAIGHT_POS:
             Target_Position -= 2
+        elif Target_Position < DXL_DEFAULT_GO_STRAIGHT_POS:
+            Target_Position += 2
         else:
             pass
         
 
-    print(f"Target Position: {Target_Position:>4}\tTarget Velocity: {Target_Velocity:>3}")
+    print(f"Target Position: {Target_Position:>-5}\tTarget Velocity: {Target_Velocity:>4}", end= "\r")
 
     bytes_Position = allocate_value_into_byte_array(Target_value= Target_Position)
     bytes_Velocity = allocate_value_into_byte_array(Target_value= Target_Velocity)
 
 
     gBulkWrite(ID_list= list_of_DXL,
-               ADDR_list= [ADDR_XM430_GOAL_POSITION, ADDR_XM430_GOAL_VELOCITY, ADDR_XM430_GOAL_POSITION, ADDR_XM430_GOAL_VELOCITY],
-               addr_LEN_list= [LEN_XM430_GOAL_POSITION, LEN_XM430_GOAL_VELOCITY, LEN_XM430_GOAL_POSITION, LEN_XM430_GOAL_VELOCITY],
-               PARAM_list= [bytes_Position, bytes_Velocity, bytes_Position, bytes_Velocity])
+               ADDR_list= [ADDR_XM430_GOAL_VELOCITY, ADDR_XM430_GOAL_VELOCITY, ADDR_XM430_GOAL_VELOCITY, ADDR_XM430_GOAL_VELOCITY, ADDR_XM430_GOAL_POSITION, ADDR_XM430_GOAL_POSITION],
+               addr_LEN_list= [LEN_XM430_GOAL_POSITION, LEN_XM430_GOAL_VELOCITY, LEN_XM430_GOAL_VELOCITY, LEN_XM430_GOAL_VELOCITY, LEN_XM430_GOAL_POSITION, LEN_XM430_GOAL_POSITION],
+               PARAM_list= [bytes_Velocity, bytes_Velocity, bytes_Velocity, bytes_Velocity, bytes_Position, bytes_Position])
     
     '''
     dxl_addparam_result = groupBulkWrite.addParam(DXL1_ID, ADDR_XM430_GOAL_POSITION, LEN_XM430_GOAL_POSITION, bytes_Position)
